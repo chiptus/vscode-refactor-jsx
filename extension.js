0e7d06d56cc4c4c32d860ff6447320e084a34826
chiptus <chiptus@gmail.com>
2017-10-05 19:48:48 -0700

    change command name

--- extension.js
+++ extension.js
@@ -13,7 +13,7 @@ function activate(context) {
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'extension.sayHello',
+    'extension.extractJsx',
     function() {
       // The code you place here will be executed every time your command is executed
       const editor = vscode.window.activeTextEditor;
