9343f3f557cae7f4c11c901ec57569c47487e3ed
Chaim Lando <chiptus@gmail.com>
2018-07-02 17:52:11 +0300

    add tests

--- src/test.js
+++ src/test.js
@@ -16,34 +16,40 @@ describe('returns an error if not jsx or invalid jsx', () => {
 });
 
 describe('returns shape of {replacement, component}', () => {
-  const returns = replace(name, `<div />`);
-  expect(typeof returns).toBe('object');
-  expect(typeof returns.replacement).toBe('string');
-  expect(typeof returns.component).toBe('string');
+  test('test', () => {
+    const returns = replace(name, `<div />`);
+    expect(typeof returns).toBe('object');
+    expect(typeof returns.replacement).toBe('string');
+    expect(typeof returns.component).toBe('string');
+  });
 });
 
 describe('tests simple component', () => {
-  const code = `
+  test('test', () => {
+    const code = `
     <div>Hello</div>
-  `;
-  const name = 'test';
-  const expected = {
-    replacement: `<Test />`,
-    component: `() => <div>Hello</div>;`,
-  };
-  const returns = replace(name, code);
-  expect(returns.replacement).toBe(expected.replacement);
-  expect(returns.component).toBe(expected.component);
+    `;
+    const name = 'test';
+    const expected = {
+      replacement: `<Test />`,
+      component: `() => <div>Hello</div>;`,
+    };
+    const returns = replace(name, code);
+    expect(returns.replacement).toBe(expected.replacement);
+    expect(returns.component).toBe(expected.component);
+  });
 });
 
 describe('test component with a single function attribute', () => {
-  const expected = {
-    replacement: '<Test t={() => t} />',
-    component: '({t}) => <div t={t} />;',
-  };
-  const returns = replace(`test`, '<div t={() => t} />');
-  expect(returns.replacement).toBe(expected.replacement);
-  expect(returns.component).toBe(expected.component);
+  test('test', () => {
+    const expected = {
+      replacement: '<Test t={() => t} />',
+      component: '({t}) => <div t={t} />;',
+    };
+    const returns = replace(`test`, '<div t={() => t} />');
+    expect(returns.replacement).toBe(expected.replacement);
+    expect(returns.component).toBe(expected.component);
+  });
 });
 
 function expectError(obj) {
